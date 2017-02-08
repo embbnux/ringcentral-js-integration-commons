84670e8aeb778f52b1f1092121d6f6a6327335c7
embbnux <embbnux.ji@ringcentral.com>
2017-02-08 12:33:35 +0800

    remove comment

--- scripts/start_release.sh
+++ scripts/start_release.sh
@@ -2,8 +2,8 @@
 
 if [[ $(git status -s) != '' ]]
   then
-  # git config user.email "integrations@ringcentral.com" &> /dev/null
-  # git config user.name "RingCentral Integrations Team" &> /dev/null
+  git config user.email "integrations@ringcentral.com" &> /dev/null
+  git config user.name "RingCentral Integrations Team" &> /dev/null
   git add --all . &> /dev/null
   git commit -m "released at $(date)" &> /dev/null
   git push origin latest -f &> /dev/null
