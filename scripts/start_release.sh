08e9bb8afd0dfdabe5059c61f2a002a600718603
embbnux <embbnux.ji@ringcentral.com>
2017-02-08 11:32:33 +0800

    travis auto publish

--- scripts/start_release.sh
+++ scripts/start_release.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-#clone latest branch
-# git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
-npm run release
-
-cd release
-
 if [[ $(git status -s) != '' ]]
   then
   # git config user.email "integrations@ringcentral.com" &> /dev/null
