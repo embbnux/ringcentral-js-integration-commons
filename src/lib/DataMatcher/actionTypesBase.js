39ed62b3a4ab5053557f88a313944ef1511cd334
embbnux <embbnux@embbnux.com>
2017-02-22 11:06:37 +0800

    add data matcher module

---
+++ src/lib/DataMatcher/actionTypesBase.js
@@ -0,0 +1,12 @@
+import Enum from '../Enum';
+
+export default new Enum([
+  'init',
+  'initSuccess',
+  'resetSuccess',
+  'match',
+  'quietMatch',
+  'matchSuccess',
+  'matchError',
+  'cleanUp',
+]);
