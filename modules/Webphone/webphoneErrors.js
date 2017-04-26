47e1bb52b60954c9ad674d109841bb85275f5bfa
embbnux <embbnux@embbnux.com>
2017-04-26 17:00:38 +0800

    released at Wed Apr 26 17:00:38 CST 2017

--- modules/Webphone/webphoneErrors.js
+++ modules/Webphone/webphoneErrors.js
@@ -10,5 +10,5 @@ var _Enum2 = _interopRequireDefault(_Enum);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = new _Enum2.default(['connectFailed', 'browserNotSupported', 'webphoneCountOverLimit', 'notOutboundCallWithoutDL', 'getSipProvisionError']);
+exports.default = new _Enum2.default(['connectFailed', 'browserNotSupported', 'webphoneCountOverLimit', 'notOutboundCallWithoutDL', 'getSipProvisionError'], 'webphone');
 //# sourceMappingURL=webphoneErrors.js.map
